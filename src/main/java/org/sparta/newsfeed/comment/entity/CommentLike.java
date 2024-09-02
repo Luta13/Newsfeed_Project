f63d924ef8218c5e93b889442d672d1439e3af7e
MinJun Song <j34679@naver.com>
2024-09-02 20:33:11 +0900

    boardLike and commentLike entity

--- src/main/java/org/sparta/newsfeed/comment/entity/CommentLike.java
+++ src/main/java/org/sparta/newsfeed/comment/entity/CommentLike.java
@@ -1,4 +1,23 @@
 package org.sparta.newsfeed.comment.entity;
 
+
+import jakarta.persistence.*;
+import org.sparta.newsfeed.user.entity.User;
+
+@Table(name = "comment_like")
+@Entity
 public class CommentLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentLikeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+
+
 }
