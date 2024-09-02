e7f8a24dfcf0e683cf5322b7b9aba84894b1474b
LJH <skRtl365@gmail.com>
2024-09-02 20:46:21 +0900

    [댓글 엔티티 기초 세팅] - 댓글 엔티티에 레이지 로딩 설정 추가 - Timestamped 엔티티 상속으로 타임스탬프 필드 통합 - build.gradle 파일 및 의존성 설정 점검 필요

--- src/main/java/org/sparta/newsfeed/comment/entity/Comment.java
+++ src/main/java/org/sparta/newsfeed/comment/entity/Comment.java
@@ -1,4 +1,37 @@
-package org.sparta.newsfeed.comment.entity;
+package main.java.org.sparta.newsfeed.comment.entity;
 
-public class Comment {
+import org.sparta.newsfeed.board.entity.Board;
+import org.sparta.newsfeed.user.entity.User;
+import org.sparta.newsfeed.common.entity.Timestamped;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @OneToMany(mappedBy = "comment", fetch = FetchType.LAZY)
+    private List<CommentLike> commentLikeList;
 }
