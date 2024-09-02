1623de96fe7f473da22b89d9dcac3234d3212dd1
chukye1216 <chukye1216@gmail.com>
2024-09-02 20:30:47 +0900

    Friend Entity Commit

--- src/main/java/org/sparta/newsfeed/friend/entity/Friend.java
+++ src/main/java/org/sparta/newsfeed/friend/entity/Friend.java
@@ -1,4 +1,24 @@
 package org.sparta.newsfeed.friend.entity;
 
+import jakarta.persistence.*;
+import org.sparta.newsfeed.user.entity.User;
+
+@Entity
+@Table(name = "friend")
 public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long relationId;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "base_email", nullable = false)
+//    private User baseEmail;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "friend_email", nullable = false)
+//    private User friendEmail;
+//
+//    @Enumerated(value = EnumType.STRING)
+//    private FriendApplyEnum applyYn;
+
 }
