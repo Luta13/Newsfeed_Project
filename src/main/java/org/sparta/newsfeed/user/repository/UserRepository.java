2f73ba876f067cf6db2bbd2616bb6e2afbfb6e11
ballqs <46590521+ballqs@users.noreply.github.com>
2024-09-03 11:42:56 +0900

    UserController 기본틀 잡기

--- src/main/java/org/sparta/newsfeed/user/repository/UserRepository.java
+++ src/main/java/org/sparta/newsfeed/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package org.sparta.newsfeed.user.repository;
 
-public class UserRepository {
+import org.sparta.newsfeed.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
