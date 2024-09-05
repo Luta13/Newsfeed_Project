5fbb8ba6d9400b4dd6cf93876b2a5b90f312e0fc
ballqs <46590521+ballqs@users.noreply.github.com>
2024-09-05 22:20:53 +0900

    MethodArgumentNotValidException 추가

--- src/main/java/org/sparta/newsfeed/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sparta/newsfeed/common/exception/GlobalExceptionHandler.java
@@ -1,13 +1,18 @@
 package org.sparta.newsfeed.common.exception;
 
+import org.sparta.newsfeed.common.dto.ResponseDto;
 import org.sparta.newsfeed.common.exception.custom.BadRequestException;
 import org.sparta.newsfeed.common.exception.custom.ConflictException;
 import org.sparta.newsfeed.common.exception.custom.ForbiddenException;
 import org.sparta.newsfeed.common.exception.custom.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,5 +34,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = NotFoundException.class)
     public ResponseEntity<ErrorResponse> NotFoundHandle(NotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND.value() , e.getMessage()));
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidHandle(MethodArgumentNotValidException e) {
+        List<String> msgList = new ArrayList<>();
+        for (int i = 0; i < e.getBindingResult().getFieldErrors().size(); i++) {
+            String msg = e.getBindingResult().getFieldErrors().get(i).getField() + "는 " + e.getBindingResult().getFieldErrors().get(i).getDefaultMessage();
+            msgList.add(msg);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(HttpStatus.BAD_REQUEST.value() , String.join(" , " , msgList)));
     }
 }
